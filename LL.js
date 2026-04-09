0ba06cd8aa3c4d154eafcf7340495849f32107a8
McLoyf <alex.ortiz242005@gmail.com>
2026-04-09 00:03:58 -0500

    Update LinkedList class
    Now it's actually a linked list and not just an array with a new nametag like my last commit. "Only" took double the time

--- LL.js
+++ LL.js
@@ -1,100 +1,126 @@
-class LinkedList {
-    //      0   1  2  3  4  5  6   7
-    list = [49, 2, 1, 3, 4, 63, 2, 4];
-
-    constructor(value, pointer) {
-        this.value = value;
-        this.pointer = pointer;
-        }
-
-        append(value) {
-            this.list.push(value);
-        }
-
-        prepend(value) {
-            this.list.reverse();
-            this.list.push(value);
-            this.list.reverse();
-        }
-
-        size() {
-            if (!(this.list)) return undefined;
-
-            return this.list.length;
-        }
-
-        head() {
-            if (!(this.list)) return undefined;
-            
-            return this.list[0];
-        }
-
-        tail() {
-            const length = this.size();
-
-            return this.list[length - 1];
-        }
-
-        at(index) {
-            if (!(this.list)) return undefined;
-
-            return this.list[index];
-        }
-
-        pop() {
-            if (!(this.list)) return undefined;
-
-            const head = this.list[0];
-
-            this.list.reverse();
-            this.list.pop();
-            this.list.reverse();
-
-            return head;
-            
-        }
-
-        contains(value) {
-            return this.list.includes(value);
-        }
-
-        findIndex(value) {
-            return this.list.indexOf(value);
-        }
-
-        toString() {
-            if (!(this.list)) return " ";
-
-            const length = this.list.length;
-
-            for (let i = 0; i <= length; i++) {
-                if (this.list[i] === undefined) {
-                    console.log("null");
-                    return;
-                }
-
-                process.stdout.write(`( ${this.list[i]} ) -> `);    // prints to console w/o newline
-            }
-        }
-    }
-
 class Node {
-    constructor(value, nextNode) {
+    constructor(value = null, nextNode = null) {
         this.value = value;
         this.nextNode = nextNode;
     }
 }
 
-const ll = new LinkedList();
+export class LinkedList {
 
+    constructor() {
+        this._head = null;
+        }
 
-// console.log("Head of list : " + ll.head());
-// console.log("Tail of list : " + ll.tail());
-// console.log("Size of list: " + ll.size());
-// console.log("Value at index 3 : " + ll.at(3));  // should be '3'
-// console.log("Value of popped head : " + ll.pop()); 
-// console.log("Does list contain 4? " + ll.contains(4)); // should be true
-// console.log("Index of 4 : " + ll.findIndex(4));
-// ll.toString();
-// ll.append(100);
-// ll.prepend(100);
+    append(value) {
+        const newNode = new Node(value);
+
+        if (!this._head) {
+            this._head = newNode;
+            return;
+        }
+
+        let current = this._head;
+        while (current.nextNode) {
+            current = current.nextNode;
+        }
+
+        current.nextNode = newNode;
+    }
+    
+    prepend(value) {
+        const newNode = new Node(value, this._head);
+        this._head = newNode;
+    }
+
+    size() {
+        let count = 0;
+        let current = this._head;
+
+        while (current) {
+            count++;
+            current = current.nextNode;
+        }
+
+        return count;
+    }
+
+    head() {
+        return this._head ? this._head.value : undefined;
+    }
+
+    tail() {
+        if (!(this._head)) return undefined;
+
+        let current = this._head;
+
+        while (current.nextNode) {
+            current = current.nextNode;
+        }
+
+        return current.value;
+    }
+
+    at(index) {
+        let current = this._head;
+        let i = 0;
+
+        while (current) {
+            if (i === index) return current.value;
+
+            current = current.nextNode;
+            i++;
+        }
+
+        return undefined;
+    }
+
+    pop() {
+        if (!(this._head)) return undefined;
+
+        const value = this._head.value;
+        this._head = this._head.nextNode;
+
+        return value;
+    }
+
+    contains(value) {
+        let current = this._head;
+
+        while(current) {
+            if (current.value === value) return true;
+
+            current = current.nextNode;
+        }
+
+        return false;
+    }
+
+    findIndex(value) {
+        let current = this._head;
+        let index = 0;
+
+        while (current) {
+            if (current.value === value) return index;
+
+            current = current.nextNode;
+            i++;
+        }
+
+        return -1;
+    }
+
+    toString() {
+        if (!(this._head)) return "";
+
+        let result = "";
+        let current = this._head;
+
+        while (current) {
+            result += `( ${current.value} ) -> `;
+            current = current.nextNode;
+        }
+
+        result += "null";
+        return result;
+    }
+}
